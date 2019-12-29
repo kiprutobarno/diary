815737a809494ebe515d0932b19b0ce4a2d41cc6
ywalakamar <barxwells@gmail.com>
2019-12-29 23:09:20 +0300

    feaure(update): Add update feature - [ ] closes #17

--- server/models/diaryitem.js
+++ server/models/diaryitem.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {}
   );
+  
   DiaryItem.associate = function(models) {
     // associations can be defined here
     DiaryItem.belongsTo(models.Diary, {
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/api/diaries", diary.createDiary);
 router.post("/api/diaries/:diaryId/items", diaryItem.create);
 router.get("/api/diaries", diary.getDiaries);
+router.put("/api/diaries/:diaryId", diary.updateDiary);
 
 export default router;
