6852ff1477a88c24878a391b943ed7f5d97d4d82
ywalakamar <barxwells@gmail.com>
2019-12-30 00:30:05 +0300

    feature(update): add update diary item feature - [ ] update `DELETE /api/diaries/:id` status code to 204 - [ ] closes #21

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,12 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/api/diaries", diary.createDiary);
-router.post("/api/diaries/:diaryId/items", diaryItem.create);
 router.get("/api/diaries", diary.getDiaries);
 router.put("/api/diaries/:diaryId", diary.updateDiary);
 router.delete("/api/diaries/:diaryId", diary.deleteDiary);
+
+router.post("/api/diaries/:diaryId/items", diaryItem.create);
+router.get("/api/diaries/:diaryId/items", diaryItem.get);
+router.put("/api/diaries/:diaryId/items/:diaryItemId", diaryItem.update);
 
 export default router;
